4f3b61f5e8a6806fd962ee9c820ee771f2907209
Talnax <rostresh@gmail.com>
2021-06-06 13:39:05 -0700

    Update sum.js

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -6,4 +6,14 @@ function sum(a, b) {
   return a + b;
 }
 
+// another variant
+/*
+let sum = (a, b) => {
+  if(isNaN(a) && isNaN(b)) {
+    throw new TypeError();
+  }
+  return a + b;
+};
+*/
+
 module.exports = sum;
